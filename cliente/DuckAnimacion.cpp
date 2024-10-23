2101ba7ace1838c09890a7a2763fdb2adffe39c5
dante <datalefi@gmail.com>
2024-10-23 16:37:24 -0300

    El pato se mueve a la derecha

--- cliente/DuckAnimacion.cpp
+++ cliente/DuckAnimacion.cpp
@@ -1,29 +1,39 @@
 #define ANCHO_IMG_DUCK_TOTAL 192
+#define DESPLAZAMIENTO 2
+#define TIEMPO_EN_DEPLAZARSE 150
 
 #include "DuckAnimacion.h"
 
 #include <SDL2/SDL.h>
 
 DuckAnimacion::DuckAnimacion(SdlWindow& window):
-        window(window),
-        movimientos("../Imagenes/DuckMovimientos.png", window),
-        srcArea(0, 0, ANCHO_IMG_DUCK, ALTO_IMG_DUCK) {}
+        window(window), movimientos("../Imagenes/DuckMovimientos.png", window) {}
+
+void DuckAnimacion::run() {
+    while (_keep_running) {
+        if (moverse_a_derecha) {
+            x_des += DESPLAZAMIENTO;
+
+            if (x_img < ANCHO_IMG_DUCK_TOTAL) {
+                x_img += ANCHO_IMG_DUCK;
+                SDL_Delay(TIEMPO_EN_DEPLAZARSE);
+
+            } else {
+                x_img = 0;
+            }
+        }
+    }
+}
 
 void DuckAnimacion::render() {
+    Area srcArea(x_img, 0, ANCHO_IMG_DUCK, ALTO_IMG_DUCK);
     Area destArea(x_des, y_des, ANCHO_IMG_DUCK, ALTO_IMG_DUCK);
     movimientos.render(srcArea, destArea);
 }
 
-void DuckAnimacion::mover_a_derecha() {
-    x_des += 10;
+void DuckAnimacion::mover_a_derecha() { moverse_a_derecha = true; }
 
-    for (int x_img = 32; x_img <= ANCHO_IMG_DUCK_TOTAL; x_img += 32) {
-        srcArea = Area(x_img, 0, ANCHO_IMG_DUCK, ALTO_IMG_DUCK);
-        window.fill();
-        this->render();
-        window.render();
-        SDL_Delay(300);
-    }
-
-    srcArea = Area(0, 0, ANCHO_IMG_DUCK, ALTO_IMG_DUCK);
+void DuckAnimacion::quieto() {
+    moverse_a_derecha = false;
+    x_img = 0;
 }
--- cliente/DuckAnimacion.h
+++ cliente/DuckAnimacion.h
@@ -4,20 +4,23 @@
 #define ANCHO_IMG_DUCK 32
 #define ALTO_IMG_DUCK 38
 
+#include "../common/common_thread.h"
 #include "Sdl/Area.h"
 #include "Sdl/SdlTexture.h"
 #include "Sdl/SdlWindow.h"
 
-class DuckAnimacion {
+class DuckAnimacion: public Thread {
 private:
     SdlWindow& window;
     SdlTexture movimientos;
-    Area srcArea;
+    bool moverse_a_derecha = false;
+    int x_img = 0;
     int x_des = 100;
     int y_des = 100;
 
 public:
     explicit DuckAnimacion(SdlWindow& window);
+    void run() override;
     void render();
     void quieto();
     void mover_a_derecha();
