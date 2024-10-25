cb0aba91dc80b72f71519d9bc6023966d123a8cc
matimorales01 <matmorales@fi.uba.ar>
2024-10-25 03:13:30 -0300

    agrego estructura del cliente

---
+++ build/cliente/CMakeFiles/sdldemo.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mati/Escritorio/tp_grupal/DuckGame/cliente/DuckAnimacion.cpp" "cliente/CMakeFiles/sdldemo.dir/DuckAnimacion.cpp.o" "gcc" "cliente/CMakeFiles/sdldemo.dir/DuckAnimacion.cpp.o.d"
+  "/home/mati/Escritorio/tp_grupal/DuckGame/cliente/Sdl/Area.cpp" "cliente/CMakeFiles/sdldemo.dir/Sdl/Area.cpp.o" "gcc" "cliente/CMakeFiles/sdldemo.dir/Sdl/Area.cpp.o.d"
+  "/home/mati/Escritorio/tp_grupal/DuckGame/cliente/Sdl/SdlException.cpp" "cliente/CMakeFiles/sdldemo.dir/Sdl/SdlException.cpp.o" "gcc" "cliente/CMakeFiles/sdldemo.dir/Sdl/SdlException.cpp.o.d"
+  "/home/mati/Escritorio/tp_grupal/DuckGame/cliente/Sdl/SdlTexture.cpp" "cliente/CMakeFiles/sdldemo.dir/Sdl/SdlTexture.cpp.o" "gcc" "cliente/CMakeFiles/sdldemo.dir/Sdl/SdlTexture.cpp.o.d"
+  "/home/mati/Escritorio/tp_grupal/DuckGame/cliente/Sdl/SdlWindow.cpp" "cliente/CMakeFiles/sdldemo.dir/Sdl/SdlWindow.cpp.o" "gcc" "cliente/CMakeFiles/sdldemo.dir/Sdl/SdlWindow.cpp.o.d"
+  "/home/mati/Escritorio/tp_grupal/DuckGame/cliente/cliente.cpp" "cliente/CMakeFiles/sdldemo.dir/cliente.cpp.o" "gcc" "cliente/CMakeFiles/sdldemo.dir/cliente.cpp.o.d"
+  "/home/mati/Escritorio/tp_grupal/DuckGame/cliente/cliente_protocolo.cpp" "cliente/CMakeFiles/sdldemo.dir/cliente_protocolo.cpp.o" "gcc" "cliente/CMakeFiles/sdldemo.dir/cliente_protocolo.cpp.o.d"
+  "/home/mati/Escritorio/tp_grupal/DuckGame/cliente/cliente_receiver.cpp" "cliente/CMakeFiles/sdldemo.dir/cliente_receiver.cpp.o" "gcc" "cliente/CMakeFiles/sdldemo.dir/cliente_receiver.cpp.o.d"
+  "/home/mati/Escritorio/tp_grupal/DuckGame/cliente/cliente_sender.cpp" "cliente/CMakeFiles/sdldemo.dir/cliente_sender.cpp.o" "gcc" "cliente/CMakeFiles/sdldemo.dir/cliente_sender.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
