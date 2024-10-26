bc0d92213b053c840aec0252834c0428083fb065
echitosto <tostoezequiel1@gmail.com>
2024-10-26 01:39:49 -0300

    feat: added collision logic

---
+++ build/cliente/CMakeFiles/sdldemo.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdldemo.dir/DuckAnimacion.cpp.o"
+  "CMakeFiles/sdldemo.dir/DuckAnimacion.cpp.o.d"
+  "CMakeFiles/sdldemo.dir/Sdl/Area.cpp.o"
+  "CMakeFiles/sdldemo.dir/Sdl/Area.cpp.o.d"
+  "CMakeFiles/sdldemo.dir/Sdl/SdlException.cpp.o"
+  "CMakeFiles/sdldemo.dir/Sdl/SdlException.cpp.o.d"
+  "CMakeFiles/sdldemo.dir/Sdl/SdlTexture.cpp.o"
+  "CMakeFiles/sdldemo.dir/Sdl/SdlTexture.cpp.o.d"
+  "CMakeFiles/sdldemo.dir/Sdl/SdlWindow.cpp.o"
+  "CMakeFiles/sdldemo.dir/Sdl/SdlWindow.cpp.o.d"
+  "libsdldemo.a"
+  "libsdldemo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdldemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
