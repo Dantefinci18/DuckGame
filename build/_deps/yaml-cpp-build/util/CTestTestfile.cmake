d999ccfc4c7a289ed5797f3995d6b8b9191ebae7
matimorales01 <matmorales@fi.uba.ar>
2024-11-19 04:09:05 -0300

    agrego patos de colores

--- build/_deps/yaml-cpp-build/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mati/Escritorio/tp_grupal/DuckGame/build/_deps/yaml-cpp-src/util
-# Build directory: /home/mati/Escritorio/tp_grupal/DuckGame/build/_deps/yaml-cpp-build/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
