70f5d5ee95449d8f29be161efa677388a81f2edd
echitosto <tostoezequiel1@gmail.com>
2024-11-19 00:47:53 -0300

    check for merging

--- build/_deps/yaml-cpp-build/util/CMakeFiles/yaml-cpp-parse_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/yaml-cpp-parse_autogen"
-  "yaml-cpp-parse_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/yaml-cpp-parse_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/_deps/yaml-cpp-build/util/CMakeFiles/yaml-cpp-sandbox_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/yaml-cpp-sandbox_autogen"
-  "yaml-cpp-sandbox_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/yaml-cpp-sandbox_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
